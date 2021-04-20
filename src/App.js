2371e5614dbdcf66e83aedc1387db5469102c4f3
Rohit Nagar <60040336+nagar2817@users.noreply.github.com>
2021-04-20 23:43:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Link, Route, Switch} from "react-router-dom"
 import HomePage from './pages/homepage/homepage.Components';
 
+// However it use a simple functionlity so for to make it simple just use a function components instead of class components.
 function App() {
   return (
     <div>
